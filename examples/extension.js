e299702ae46bfa3ea15a2d0a94498569fff26fa6
Mike Davis <mike@michaeljacobdavis.com>
2015-09-06 11:09:46 -0700

    Fix example

--- examples/extension.js
+++ examples/extension.js
@@ -1,7 +1,7 @@
 // Import the module you want
 var config = require('hubot-response-success');
 
-// Extend whatever you wany
+// Extend whatever you want
 config.response.push('success!');
 
 // Export the altered object
